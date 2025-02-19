b18c82fa6b7a37b1a701fb99b9e17aa26d1904c8
Isaque Misael <sergioisaquepb@gmail.com>
2025-02-19 17:05:45 -0300

    dell files

--- aula1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    printf("Hello, world!\n");
-    printf("Pressione Enter para continuar...");
-    getchar(); // o system pause só funciona no windows
-    return 0;
-}
